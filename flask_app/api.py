c3868ce4d209821015eed0cdbc8a94b542adfcf9
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-04-29 20:51:42 -0700

    only check the query not the arguments for the handler

--- flask_app/api.py
+++ flask_app/api.py
@@ -6,31 +6,45 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-valid_queries = ["show_connections"]
+valid_queries = ["show_connections", "show_credentials"]
 
 
 @app.route("/api", methods=["GET"])
 def handle_queries() -> str | None:
-    query = request.query_string.decode("utf-8")
+    query = request.query_string.decode("utf-8")[:request.query_string.decode("utf-8").find("=")]
     if request.method == "GET" and query in valid_queries:
         if query == "show_connections":
             return show_connections()
+        if query == "show_credentials":
+            return get_credentials(request.args["show_credentials"])
     return f"QUERY: {query}\nNot a valid query."
 
 
 def show_connections() -> dict:
+    return {"saved_connections": saved_connections}
+
+
+def get_connections() -> list:
     saved_wifi = []
-    output = {}
     show_wifi = str(
         subprocess.check_output(["ls", "/etc/NetworkManager/system-connections/"])
     )
     for i in show_wifi.split(","):
-        saved_wifi.append(
-            re.findall(r"'(.*?)'", i)[0].replace("\\n", "")
-        )  # single line parse
-    output["result"] = saved_wifi
-    return output
+        saved_wifi.append(re.findall(r"'(.*?)'", i)[0].replace("\\n", ""))
+    return saved_wifi
 
+
+def get_credentials(ssid) -> str:
+    if ssid in saved_connections:
+        return str(
+            subprocess.check_output(
+                ["cat", f"/etc/NetworkManager/system-connections/{ssid}"]
+            )
+        )
+    return f"SSID: {ssid}\nNot in saved connections."
+
+
+saved_connections = get_connections()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
