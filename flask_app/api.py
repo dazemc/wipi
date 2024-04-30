32d98e939bc164b4778bf11531f3ef55c2c9c50c
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-04-29 19:51:58 -0700

    parse subprocess output

--- flask_app/api.py
+++ flask_app/api.py
@@ -1,4 +1,5 @@
 import subprocess
+import re
 from flask import Flask, request
 from flask_cors import CORS
 
@@ -8,12 +9,13 @@ CORS(app)
 @app.route("/api", methods=["GET"])
 def show_saved_wifi() -> dict:
     if request.method == "GET":
-        saved_wifi = {}
-        result = subprocess.check_output(["ls", "/etc/NetworkManager/system-connections/"])
-        saved_wifi["result"] = {
-            "saved_connections": result,
-        }
-        return saved_wifi
+        saved_wifi = []
+        output = {}
+        show_wifi = str(subprocess.check_output(["ls", "/etc/NetworkManager/system-connections/"]))
+        for i in show_wifi.split(','):
+            saved_wifi.append(re.findall(r"'(.*?)'", i).replace("\\n"), '')  # single line parse
+        output["result"] = saved_wifi
+        return output
 
 
 if __name__ == "__main__":
