68db6c04ca85ea488b36a2ee2f24faf2da5714e0
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-04-29 20:31:13 -0700

    handle all future queries

--- flask_app/api.py
+++ flask_app/api.py
@@ -6,16 +6,30 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+valid_queries = ["show_connections"]
+
+
 @app.route("/api", methods=["GET"])
-def show_saved_wifi() -> dict:
-    if request.method == "GET":
-        saved_wifi = []
-        output = {}
-        show_wifi = str(subprocess.check_output(["ls", "/etc/NetworkManager/system-connections/"]))
-        for i in show_wifi.split(','):
-            saved_wifi.append(re.findall(r"'(.*?)'", i)[0].replace("\\n", ''))  # single line parse
-        output["result"] = saved_wifi
-        return output
+def handle_queries() -> str | None:
+    query = request.query_string.decode("utf-8")
+    if request.method == "GET" and query in valid_queries:
+        if query == "show_connections":
+            return show_connections()
+    return f"QUERY: {query}\nNot a valid query."
+
+
+def show_connections() -> dict:
+    saved_wifi = []
+    output = {}
+    show_wifi = str(
+        subprocess.check_output(["ls", "/etc/NetworkManager/system-connections/"])
+    )
+    for i in show_wifi.split(","):
+        saved_wifi.append(
+            re.findall(r"'(.*?)'", i)[0].replace("\\n", "")
+        )  # single line parse
+    output["result"] = saved_wifi
+    return output
 
 
 if __name__ == "__main__":
