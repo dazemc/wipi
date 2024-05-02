9aabf7a2d0afef67082a33b6feea594f3ab25a22
Daazed McFarland <daazedjmcfarland@gmail.com>
2024-05-01 23:00:38 -0700

    don't need to stop wpa_supplicant if the system is going to reboot

--- flask_app/ap_setup.sh
+++ flask_app/ap_setup.sh
@@ -1,5 +1,5 @@
 apt update && apt install iwd
 rm -f /etc/NetworkManager/conf.d/iwd.conf && touch /etc/NetworkManager/conf.d/iwd.conf
 echo -e "[device]\nwifi.backend=iwd" | tee /etc/NetworkManager/conf.d/iwd.conf
-systemctl stop wpa_supplicant && systemctl disable wpa_supplicant && systemctl enable iwd
+systemctl disable wpa_supplicant && systemctl enable iwd
 reboot & exit
